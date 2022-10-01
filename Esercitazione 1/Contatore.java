27161a7c3d9ca8e4d23cf9a62688c31f8ecafd96
Richard Paris <Antonino117@hotmail.it>
2022-10-01 12:03:20 +0200

    DIO PORCO

---
+++ Esercitazione 1/Contatore.java
@@ -0,0 +1,6 @@
+public class Contatore{
+    public static void main(String args[]){
+        int count = args.length;
+        System.out.println("L'utente ha inserito " + count + " argomenti.");
+    }
+}
---
+++ Esercitazione 1/Countdown.java
@@ -0,0 +1,10 @@
+public class Countdown{
+    public static void main(String args[]){
+
+        for(int i = 10; i > 0; --i){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
