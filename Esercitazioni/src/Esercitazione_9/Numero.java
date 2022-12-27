2edcae49def791be45643256cc0b176325fcacf7
Richard Paris <Antonino117@hotmail.it>
2022-12-27 19:48:56 +0100

    interface fix

--- Esercitazioni/src/Esercitazione_9/Numero.java
+++ Esercitazioni/src/Esercitazione_9/Numero.java
@@ -1,6 +1,5 @@
 package bin.es9;
 
-public abstract class Numero {
-    public abstract Numero somma(Numero... numeri);
-    public abstract Numero sottrai(Numero... numeri);
+public abstract class Numero implements Aritmetica {
+
 }
